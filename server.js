ddfe1f0b315487bfc893658066aef3ca4db643da
Fepo <f.polit13@gmail.com>
2018-11-20 19:24:44 -0300

    changin database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : 'postgresql-round-35281',
-    user : 'postgres',
-    password: 'secret',
-    database : 'postgres',
+	    host : process.env.DATABASE_URL,
+	    ssl: true,
   }
 });
 
