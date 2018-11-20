2a8735e767594347d10fa35e008c08b0e31c6872
Fepo <f.polit13@gmail.com>
2018-11-20 19:14:46 -0300

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-round-35281',
     user : 'postgres',
     password: 'secret',
     database : 'postgres',
